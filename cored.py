cabd2dd0565b6d30e310de6f17051454103a8470
DJCzubi <patryk.jap@gmail.com>
2024-08-17 19:30:11 +0200

    Reder split to task for faster render

--- cored.py
+++ cored.py
@@ -1,31 +1,71 @@
-from symtable import Class
+from idlelib.run import manage_socket
 
+import main
 from main import *
 import multiprocessing as mp
+from multiprocessing import Manager
+from PIL import Image
 
+slices = [5,5]
+slice_size = (int(main.simulation_size[0] / slices[0]), int(main.simulation_size[1] / slices[1]))
+#TODO: make it so maximum like 10 proceses get initiated at once
+class ImageSlice:
+    def __init__(self, start_pos, image):
+        self.start_pos = start_pos
+        self.image = image
 
-
-def render_gravity_basins(static_bodies: List[StaticBody]):
-    slice_size = (256, 144)
-    def render_slice(start_x, end_x, start_y, end_y, image):
-
+def render_slice(queue, start_x, end_x, start_y, end_y, static_bodies, save=False):
+    try:
+        image_slice = Image.new('RGB', slice_size, color='black')
+        pixels = image_slice.load()
         for x in range(start_x, end_x):
             for y in range(start_y, end_y):
                 crashing_body = get_crashing_body(static_bodies, [x, y])
                 if crashing_body is None:
                     continue
-                image.putpixel((x, y), crashing_body.color)
+                pixels[x-start_x, y-start_y] = crashing_body.color
+
+        output = ImageSlice([start_x, start_y], image_slice)
+        queue.put(output)
+
+    except Exception as e:
+        print(f"Error in render_slice: {e}")
+        queue.put(None)  # Put a sentinel value to indicate an error
+    finally:
+        queue.put(None)
 
 
+
+def render_gravity_basins(static_bodies: List[StaticBody], output_image: Image):
+    queues = []
     processes = []
-
-    for x_slice in range(simulation_size[0] / slice_size[0]):
-        for y_slice in range(simulation_size[1] / slice_size[1]):
-            args = (x_slice*slice_size[0], ((x_slice+1)*slice_size[0]-1),
-                    y_slice*slice_size[1], ((y_slice+1)*slice_size[1]-1))
+    manager = Manager()
+    save = True
+    for x_slice in range(slices[0]):
+        for y_slice in range(slices[1]):
+            queue = manager.Queue()
+            queues.append(queue)
+            args = (queue,
+                    x_slice*slice_size[0], ((x_slice+1)*slice_size[0]),
+                    y_slice*slice_size[1], ((y_slice+1)*slice_size[1]),
+                    static_bodies, save)
             processes.append(mp.Process(target=render_slice,args=args))
             processes[-1].start()
+            save = False
 
+    while any(p.is_alive() for p in processes):
+        running_processes = sum(p.is_alive() for p in processes)
+        print(f"\rSlices rendered: {len(processes)-running_processes}/{len(processes)}", end='', flush=True)
+        time.sleep(1)
+
+    image_slices = []
     for p in processes:
         p.join()
+
+    for q in queues:
+        image_slices.append(q.get())
+
+    for image_slice in image_slices:
+        output_image.paste(image_slice.image, image_slice.start_pos)
+
 
--- main.py
+++ main.py
@@ -4,27 +4,32 @@ import PIL.ImageDraw2
 # import cored
 from PIL import Image, ImageDraw
 from typing import List
-import platform; print(platform.python_implementation())
+import platform
+
+from numpy import dtype
+
+import cored
+
 import signal
 import numpy as np
 import sys
 import time
 #TODO: show time remaining
 
-simulation_size = (2500, 2500)
+simulation_size = (1100, 900)
 
 image = Image.new('RGB', simulation_size, color='black')
 draw = ImageDraw.Draw(image)
-max_steps = 10000000
-step_size = 0.1
+max_steps = 10000
+step_size = 0
 keep_velocity = 0.999
-
+GRAVITY_CONSTANT = 10
+PARTICLE_MASS = 0.4
 force_stop = False
 
 class StaticBody:
     def __init__(self,x,y,mass, color, density=0.1):
-        self.x = x
-        self.y = y
+        self.pos = np.array([x,y])
         self.mass = mass
         self.color = color
         self.density = density
@@ -32,16 +37,25 @@ class StaticBody:
 
         print(f"mass:{self.mass} radius:{self.radius}")
 
-    def get_distance(self, x, y):
-        return math.sqrt((x - self.x)**2 + (y - self.y)**2)
+    def get_distance(self, pos):
+        return np.linalg.norm(self.pos - pos)
 
     def get_distance_squared(self, x, y):
         return (x - self.x)**2 + (y - self.y)**2
 
-def render_bodies(static_bodies: List[StaticBody]):
-    for body in static_bodies:
+    @property
+    def x(self):
+        return self.pos[0]
+    @property
+    def y(self):
+        return self.pos[1]
 
-        draw.circle([body.x,body.y],body.radius,(0,0,0))
+def render_bodies(static_bodies: List[StaticBody], black = False):
+    for body in static_bodies:
+        if black:
+            draw.circle(body.pos, body.radius,(0,0,0))
+        else:
+            draw.circle(body.pos, body.radius, body.color)
 
 
 def render_gravity_basins(static_bodies: List[StaticBody]):
@@ -55,8 +69,9 @@ def render_gravity_basins(static_bodies: List[StaticBody]):
             if force_stop:
                 return
 
-            if current_pixel % 100 == 0:
-                print(f"\rPixel: {current_pixel}/{simulation_size[0] * simulation_size[1]}  {round(current_pixel / (simulation_size[0] * simulation_size[1]) * 100)}%    black pixels: {black_pixels}", end='', flush=True)
+            print(f"\rPixel: {current_pixel}/{simulation_size[0] * simulation_size[1]}  {round(current_pixel / (simulation_size[0] * simulation_size[1]) * 100)}%    black pixels: {black_pixels}", end='', flush=True)
+            if current_pixel % 500 == 0:
+                image.save("gravity_wells_friction_smallstep4.png")
 
             crashing_body = get_crashing_body(static_bodies, [x, y])
 
@@ -68,63 +83,73 @@ def render_gravity_basins(static_bodies: List[StaticBody]):
             current_pixel += 1
 
 def get_crashing_body(static_bodies: List[StaticBody], starting_position: list[2]) -> StaticBody:
-    pos = starting_position
-
+    pos = np.array(starting_position, dtype=float)
+    velocity = np.array([0,0], dtype=float)
     for body in static_bodies:
-        if pos[0] == body.x and pos[1] == body.y:
+        if pos[0] == body.pos[0] and pos[1] == body.pos[1]:
             return body
 
-    velocity = [0, 0]
-
     for step in range(max_steps):
-
-        acting_force = np.array([0,0])
+        force = np.array([0,0], dtype=float)
         for body in static_bodies:
-            distance_squared = body.get_distance_squared(pos[0], pos[1])
-            force_x = (body.x-pos[0])/distance_squared * body.mass
-            force_y = (body.y-pos[1])/distance_squared * body.mass
-
-            acting_force[0] += force_x
-            acting_force[1] += force_y
-            if distance_squared <= body.radius**2:
+            force += gravity_force(body, pos)
+            if body.get_distance_squared(pos[0], pos[1]) <= body.radius**2:
                 return body
 
-        velocity[0] = velocity[0] * keep_velocity + acting_force[0]
-        velocity[1] = velocity[1] * keep_velocity + acting_force[1]
-        pos[0] += velocity[0]
-        pos[1] += velocity[1]
+        velocity += force/PARTICLE_MASS
+        pos += velocity
 
     return None
 
 
 
-def render_trajectory(static_bodies: List[StaticBody], starting_position: list[2]):
-    pos = starting_position
-    velocity = [0,0]
+def render_trajectory(static_bodies: List[StaticBody], starting_position: list[2], radius = 1):
+    pos = np.array(starting_position, dtype=float)
+    velocity = np.array([0,0], dtype=float)
     for step in range(max_steps):
+        force = np.array([0,0], dtype=float)
+        for body in static_bodies:
+            force += gravity_force(body, pos)
+            if body.get_distance_squared(pos[0], pos[1]) <= body.radius**2:
+                return
+
+        velocity += force/PARTICLE_MASS
+        pos += velocity
+
+
         if 0 <= pos[0] <= simulation_size[0] and 0 <= pos[1] <= simulation_size[1]:
             image.putpixel((int(pos[0]), int(pos[1])), (255,255,255))
+            draw.circle(pos, radius, (255,255,255))
 
-        acting_force = [0,0]
-        for body in static_bodies:
-            distance_squared = body.get_distance_squared(pos[0], pos[1])
-            force_x = (body.x-pos[0])/distance_squared * body.mass * step_size
-            force_y = (body.y-pos[1])/distance_squared * body.mass * step_size
 
-            acting_force[0] += force_x
-            acting_force[1] += force_y
-            if distance_squared <= body.radius**2:
-                return
-        velocity[0] += acting_force[0]
-        velocity[1] += acting_force[1]
-        pos[0] += velocity[0]
-        pos[1] += velocity[1]
+def net_gravity_force(bodies: List[StaticBody], position: np.ndarray) -> np.ndarray:
+    net_force = np.zeros(2, dtype=float)
+    for body in bodies:
+        net_force += gravity_force(body, position)
+
+    return net_force
+
+
+
+
+def gravity_force(body: StaticBody, pos: np.ndarray) -> np.ndarray:
+    distance = body.get_distance(pos)
+
+    direction_vector = (body.pos - pos)/distance
+
+    magnitude = GRAVITY_CONSTANT * body.mass / (distance**2)
+
+    return direction_vector * magnitude
+
+
 
 def on_exit(sig, frame):
     global force_stop
     force_stop = True
 
 if __name__ == "__main__":
+    print(platform.python_implementation())
+
     signal.signal(signal.SIGINT, on_exit)
     signal.signal(signal.SIGTERM, on_exit)
 
@@ -132,11 +157,12 @@ if __name__ == "__main__":
     #           StaticBody(250, 250, 5, (41,255,31)),
     #           StaticBody(150, 150, 12,(10,14, 241)),
     #           StaticBody(450, 150, 14,(200,14, 241))]
-    bodies = [StaticBody(100, 100, 2000, (255,39,12)),
-              StaticBody(250, 250, 2000, (41,255,31)),
-              StaticBody(400, 400, 2000,(10,14, 241))]
-    render_gravity_basins(bodies)
-    render_bodies(bodies)
-    # render_trajectory(bodies, starting_position=[1400,300])
-    image.save("gravity_wells_friction_smallstep.png")
+    bodies = [StaticBody(75, 75, 200, (255,39,12)),
+              StaticBody(550, 45, 200, (41,255,31)),
+              StaticBody(800, 400, 200,(10,14, 241))]
+    cored.render_gravity_basins(bodies, image)
+    render_bodies(bodies, black=True)
+    # render_trajectory(bodies, starting_position=[1400,300], radius=5)
+    image.save("gravity_wells_friction_smallstep4.png")
+    image.show()
 
